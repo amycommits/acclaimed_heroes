34ef837f42c58a4360246231d58ccad6f751a362
duaimei <adetwiler@optoro.com>
2020-10-03 10:31:38 -0400

    setup seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "http"
+User.delete_all
+
+response = JSON.parse(HTTP.get('https://randomuser.me/api/?results=50').body)['results']
+# this particular API will either allow you to make multiple requests, or search for gender
+response = response.filter {|user| user['gender'] == 'female'}
+
+response.each do |woman|
+  User.create!(
+    email: woman['email'],
+    first_name: woman['name']['first'],
+    last_name: woman['name']['last'],
+    dob: woman['dob']['date'],
+    phone: woman['phone'],
+    profile_picture: woman['picture']['medium'],
+    latitude: woman['location']['coordinates']['latitude'],
+    longitude: woman['location']['coordinates']['longitude'],
+    password: woman['login']['password'].length >= 6 ? woman['login']['password'] : '123456'
+  )
+end
