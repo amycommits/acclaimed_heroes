af417c91984b00d75c30a2af68cd0c2a3fc86ae0
duaimei <adetwiler@optoro.com>
2020-10-03 12:21:42 -0400

    added the basic badge functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_033132) do
+ActiveRecord::Schema.define(version: 2020_10_03_161521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.string "remote_id"
+    t.string "badge_url"
+    t.string "image_url"
+    t.json "skill"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
